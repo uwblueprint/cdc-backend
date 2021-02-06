1c505362183755ed25d7006dbd8aabcbfe511862
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2021-02-06 13:25:11 -0500

    Initial script creating a couple tables (using psycopg) (#3)
    * Update requirements file to include psycopg.
    
    * Initial work on a create tables script.
    
    * add stateless script run
    
    Co-authored-by: jaydhulia <jaydhulia@hotmail.com>

---
+++ scripts/create-tables.py
@@ -0,0 +1,42 @@
+import psycopg2
+
+hostname = "localhost"
+database = "postgres_cdc_dev"
+user = "postgres"
+password = ""
+
+conn = psycopg2.connect(host=hostname, database=database, user=user, password=password)
+
+table_creation_commands = [
+    """
+        CREATE TABLE IF NOT EXISTS asset (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(256) NOT NULL,
+            s3_key VARCHAR(256) NOT NULL,
+            obj_type VARCHAR(50) NOT NULL
+        )
+    """,
+    """
+        CREATE TABLE IF NOT EXISTS scenario (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(256) NOT NULL,
+            friendly_name VARCHAR(256) NOT NULL,
+            description VARCHAR(256) NOT NULL,
+            scene_ids BIGINT[] NOT NULL,
+            is_published BOOLEAN NOT NULL,
+            is_archived BOOLEAN NOT NULL,
+            is_previewable BOOLEAN NOT NULL,
+            publish_link VARCHAR(256),
+            preview_link VARCHAR(256),
+            expected_solve_time VARCHAR(20)
+        )
+    """,
+]
+
+cur = conn.cursor()
+
+for command in table_creation_commands:
+    cur.execute(command)
+
+cur.close()
+conn.commit()

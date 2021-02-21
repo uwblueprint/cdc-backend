86aedf3803f1a78d5b44b1cfc50790096262aa27
jaydhulia <jaydhulia@hotmail.com>
2021-02-21 12:29:50 -0700

    fix table inconsistency

--- app/models/scene.py
+++ app/models/scene.py
@@ -9,18 +9,18 @@ class Scene(Base):
     id = Column(Integer, primary_key=True)
     name = Column(Text, nullable=False)
     description = Column(Text, nullable=False)
-    object_ids = Column(ARRAY(BigInteger), nullable=False)
+    objects_id = Column(ARRAY(BigInteger), nullable=False)
     position = Column(ARRAY(Float), nullable=False)
     scale = Column(ARRAY(Float), nullable=False)
     rotation = Column(ARRAY(Float), nullable=False)
     background_id = Column(BigInteger, ForeignKey("asset.id"), nullable=False)
 
     def __init__(
-        self, name, description, object_ids, position, scale, rotation, background_id
+        self, name, description, objects_id, position, scale, rotation, background_id
     ):
         self.name = name
         self.description = description
-        self.object_ids = object_ids
+        self.objects_id = objects_id
         self.position = position
         self.scale = scale
         self.rotation = rotation

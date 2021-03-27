ea42fb6a6ff1e484d4e31a849da261aae517ad12
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2021-03-27 15:41:26 -0400

    Jigsaw Prototype (#55)
    * Working jigsaw prototype.
    
    * Remove unnecessary third parameter.

---
+++ samples/jigsaw-prototype.py
@@ -0,0 +1,17 @@
+import cv2
+
+img = cv2.imread("images/whmis_labels.png")
+rows = 3
+cols = 3
+pixelXscale = int(img.shape[0] / rows)
+pixelYscale = int(img.shape[1] / cols)
+
+for r in range(1, rows + 1):
+    for c in range(1, cols + 1):
+        cv2.imwrite(
+            f"images/img{r}_{c}.png",
+            img[
+                (r - 1) * pixelXscale : r * pixelXscale,  # noqa: E203
+                (c - 1) * pixelYscale : c * pixelYscale,  # noqa: E203
+            ],
+        )

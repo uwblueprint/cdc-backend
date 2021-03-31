f48c270ccd187ed1d9284aabed47d42c21197706
Dhruvin Balar <35475783+drbalar@users.noreply.github.com>
2021-03-31 01:09:19 -0400

    Popup function and html structure. (#57)
    * Popup function and html structure.
    
    * removed read-room component
    
    * removed physics/gravity stuff
    
    * renamed chair to prism
    
    * moved popup files into `samples/popup` dir
    
    * Added closePopup.js and openPopup.js components and moved them all into components folder
    
    * Listening for loading eventlistener before adding click listener.
    
    * Moved return button json into its html tag
    
    * moved popup files into `samples/popup` dir

---
+++ samples/popup/components/openPopup.js
@@ -0,0 +1,60 @@
+AFRAME.registerComponent("open-popup", {
+  schema: {
+    jsonData: {
+      parse: JSON.parse,
+      stringify: JSON.stringify,
+    },
+  },
+  multiple: true,
+  /**
+   * Initial creation and setting of the mesh.
+   */
+  init: function () {
+    const data = this.data.jsonData;
+    const el = this.el;
+
+    // Create geometry.
+    this.geometry = new THREE.BoxBufferGeometry(
+      data.width,
+      data.height,
+      data.depth
+    );
+
+    // Create material.
+    this.material = new THREE.MeshStandardMaterial({ color: data.color });
+
+    // Create mesh.
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+    el.object3D.position.set(data.x, data.y, data.z);
+    // Set mesh on entity.
+    el.setObject3D("mesh", this.mesh);
+    el.addEventListener("loaded", function (e) {
+      if (e.target === el) {
+        // Add event listener for closing (returning) on mouse click.
+        el.addEventListener("click", function () {
+          addEntityToBlackboard(JSON.stringify(data.blackboardData));
+        });
+      }
+    });
+  },
+});
+
+// TODO: Add support for multiple entity creation
+function addEntityToBlackboard(componentData) {
+  var componentDataParsed = JSON.parse(componentData);
+  var blackboardEl = document.querySelector("#blackboard");
+  var entityEl = document.createElement("a-entity");
+  entityEl.setAttribute(
+    componentDataParsed.componentType,
+    "jsonData",
+    JSON.stringify(componentDataParsed.jsonData)
+  );
+  entityEl.addEventListener("loaded", function (e) {
+    if (e.target === entityEl) {
+      var popupCameraEl = document.querySelector("#popup-camera");
+      popupCameraEl.setAttribute("camera", "active", true);
+    }
+  });
+  blackboardEl.appendChild(entityEl);
+}

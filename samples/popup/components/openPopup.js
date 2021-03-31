0ca80705308155a5ea5410cd6ee83ba11eaec02e
Dhruvin Balar <dhruvinb123@gmail.com>
2021-03-30 22:25:07 -0400

    Listening for loading eventlistener before adding click listener.

--- samples/popup/components/openPopup.js
+++ samples/popup/components/openPopup.js
@@ -10,9 +10,6 @@ AFRAME.registerComponent("open-popup", {
    * Initial creation and setting of the mesh.
    */
   init: function () {
-    console.log("RANNNNNNNNNNNNNNN");
-    console.log("RANNNNNNNNNNNNNNN");
-    console.log("RANNNNNNNNNNNNNNN");
     const data = this.data.jsonData;
     const el = this.el;
 
@@ -32,12 +29,15 @@ AFRAME.registerComponent("open-popup", {
     el.object3D.position.set(data.x, data.y, data.z);
     // Set mesh on entity.
     el.setObject3D("mesh", this.mesh);
-
-    // Add event listener for closing (returning) on mouse click.
-    el.addEventListener("click", function () {
-      addEntityToBlackboard(JSON.stringify(data.blackboardData));
-      var popupCameraEl = document.querySelector("#popup-camera");
-      popupCameraEl.setAttribute("camera", "active", true);
+    el.addEventListener("loaded", function (e) {
+      if (e.target === el) {
+        // Add event listener for closing (returning) on mouse click.
+        el.addEventListener("click", function () {
+          addEntityToBlackboard(JSON.stringify(data.blackboardData));
+          var popupCameraEl = document.querySelector("#popup-camera");
+          popupCameraEl.setAttribute("camera", "active", true);
+        });
+      }
     });
   },
 });

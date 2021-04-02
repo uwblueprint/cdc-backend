46ab41b89f534affbb9191ff168117fa8c194627
Jay Dhulia <46459060+jaydhulia@users.noreply.github.com>
2021-04-02 13:31:27 -0600

    Cleanup keypad.js

--- app/public/static/js/components/keypad.js
+++ app/public/static/js/components/keypad.js
@@ -39,23 +39,6 @@ AFRAME.registerComponent("keypad", {
         }
       }
     });
-
-    // // Create geometry.
-    // this.geometry = new THREE.BoxBufferGeometry(
-    //   data.width,
-    //   data.height,
-    //   data.depth
-    // );
-    //
-    // // Create material.
-    // this.material = new THREE.MeshStandardMaterial({ color: data.color });
-    //
-    // // Create mesh.
-    // this.mesh = new THREE.Mesh(this.geometry, this.material);
-    //
-    // el.object3D.position.set(data.x, data.y, data.z);
-    // // Set mesh on entity.
-    // el.setObject3D("mesh", this.mesh);
   },
 });
 

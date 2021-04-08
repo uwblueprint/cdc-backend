6d6f6a81256ff189b338f2b41947050d68d7920f
vivianliu0 <56518575+vivianliu0@users.noreply.github.com>
2021-04-07 23:29:16 -0400

    Aframe Text Pane (#63)
    * WIP for text-panes
    
    * WIP text pane
    
    * added nav buttons
    
    * added nav buttons
    
    * Addressed PR comments
    
    * removed unneeded file

---
+++ samples/text_panes/components/text-box.js
@@ -0,0 +1,39 @@
+AFRAME.registerComponent("text-box", {
+  schema: {
+    jsonData: {
+      parse: JSON.parse,
+      stringify: JSON.stringify,
+    },
+  },
+  multiple: true,
+  /**
+   * Initial creation and setting of the mesh.
+   */
+  init: function () {
+    const data = this.data.jsonData;
+    const el = this.el;
+
+    // Create geometry.
+    this.geometry = new THREE.PlaneGeometry(data.width, data.height);
+
+    // Create material.
+    this.material = new THREE.MeshStandardMaterial({ color: data.color });
+
+    // Create mesh.
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+    el.object3D.position.set(data.x, data.y, data.z);
+    // Set mesh on entity.
+    el.setObject3D("mesh", this.mesh);
+
+    // Create textLabel
+    this.textLabel = document.createElement("a-text");
+    this.textLabel.setAttribute("id", "text-nav");
+    this.textLabel.setAttribute("value", data.text);
+    this.textLabel.setAttribute("negate", "true");
+    this.textLabel.setAttribute("scale", "2 2 1");
+    this.textLabel.setAttribute("color", "black");
+    this.textLabel.setAttribute("align", "center");
+    this.el.appendChild(this.textLabel);
+  },
+});
---
+++ samples/text_panes/components/text_pane.js
@@ -0,0 +1,73 @@
+AFRAME.registerComponent("text_pane", {
+  schema: {
+    jsonData: {
+      parse: JSON.parse,
+      stringify: JSON.stringify,
+    },
+  },
+
+  multiple: true,
+
+  init: function () {
+    const data = this.data.jsonData;
+    const el = this.el;
+
+    // Create textLabel
+    this.textLabel = document.createElement("a-text");
+    this.textLabel.setAttribute("id", "text");
+    this.textLabel.setAttribute("value", data.text[data.currPosition]);
+    this.textLabel.setAttribute("negate", "true");
+    this.textLabel.setAttribute("scale", "2 2 1");
+    this.textLabel.setAttribute("position", "-4.9 0 0.25");
+    this.el.appendChild(this.textLabel);
+
+    if (data.text.length > 1) {
+      const leftNavProp =
+        '{"width": "1.5", "height": "1.5", "depth": "0.005", "color": "white", "x": "-8", "y": "0", "z": "0.005", "text": "Prev"}';
+      const rightNavProp =
+        '{"width": "1.5", "height": "1.5", "depth": "0.005", "color": "white", "x": "8", "y": "0", "z": "0.005", "text": "Next"}';
+      const textLabelConst = this.textLabel;
+
+      // Create left nav button
+      this.leftNav = document.createElement("a-entity");
+      this.leftNav.setAttribute("id", "nav-button-left");
+      this.leftNav.setAttribute("text-box", "jsonData", leftNavProp);
+      this.el.appendChild(this.leftNav);
+
+      // Create right nav button
+      this.rightNav = document.createElement("a-entity");
+      this.rightNav.setAttribute("id", "nav-button-right");
+      this.rightNav.setAttribute("text-box", "jsonData", rightNavProp);
+      this.el.appendChild(this.rightNav);
+
+      const rightNavConst = this.rightNav;
+      this.leftNav.addEventListener("click", function () {
+        if (data.currPosition != 0) {
+          --data.currPosition;
+          textLabelConst.setAttribute("value", data.text[data.currPosition]);
+          rightNavConst.firstChild.setAttribute("value", "Next");
+        }
+      });
+
+      this.rightNav.addEventListener("click", function () {
+        if (data.currPosition == data.text.length - 2) {
+          rightNavConst.firstChild.setAttribute("value", "Done");
+        }
+
+        if (data.currPosition != data.text.length - 1) {
+          ++data.currPosition;
+          textLabelConst.setAttribute("value", data.text[data.currPosition]);
+        } else {
+          closeTextPane();
+        }
+      });
+    }
+  },
+});
+
+function closeTextPane() {
+  var textPaneEl = document.querySelector("#text_pane");
+  while (textPaneEl.firstChild) {
+    textPaneEl.removeChild(textPaneEl.lastChild);
+  }
+}

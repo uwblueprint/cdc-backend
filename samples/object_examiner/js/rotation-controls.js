9fe305a617d3f52c3a08ec6760cbca100764efcc
Dhruvin Balar <dhruvinb123@gmail.com>
2021-04-13 22:10:36 -0400

    Created a component rotation-controls.js

---
+++ samples/object_examiner/js/rotation-controls.js
@@ -0,0 +1,65 @@
+let { THREE } = AFRAME;
+let { TransformControls } = THREE;
+
+AFRAME.registerComponent("rotation-controls", {
+  init: function () {
+    let { el } = this;
+    let self = this;
+    let obj = el.object3D;
+    let sceneEl = el.sceneEl;
+    let scene = sceneEl.object3D;
+    let renderer = sceneEl.renderer;
+    let camera, controls;
+
+    let cameraEl = document.querySelector("[camera]");
+
+    let controlsSetup = function () {
+      camera = sceneEl.camera;
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(renderer.domElement);
+
+      controls = new TransformControls(camera, renderer.domElement);
+      controls.addEventListener("change", render);
+      controls.addEventListener("dragging-changed", function (event) {
+        cameraEl.setAttribute("look-controls", "enabled", `${!event.value}`);
+      });
+      controls.attach(obj);
+      scene.add(controls);
+
+      window.addEventListener("resize", onWindowResize);
+
+      controls.setMode("rotate");
+      Object.assign(self, {
+        camera,
+        controls,
+      });
+    };
+
+    if (sceneEl.camera === undefined) {
+      sceneEl.addEventListener("camera-set-active", controlsSetup, {
+        once: true,
+      });
+    } else {
+      controlsSetup();
+    }
+
+    function onWindowResize() {
+      const aspect = window.innerWidth / window.innerHeight;
+
+      camera.aspect = aspect;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(window.innerWidth, window.innerHeight);
+
+      render();
+    }
+
+    function render() {
+      renderer.render(scene, camera);
+    }
+  },
+  remove: function () {
+    this.controls.dispose();
+  },
+});

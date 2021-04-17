6f1c0a0e1fb860b5a61895ca28f3c79cacd42cea
Vivian <vivianliu1@live.com>
2021-04-17 16:36:30 -0400

    bug fix

--- app/public/static/js/components/keypad.js
+++ app/public/static/js/components/keypad.js
@@ -15,9 +15,9 @@ AFRAME.registerComponent("keypad", {
     const el = this.el;
     el.setAttribute(
       "super-keyboard",
-      "imagePath:/static/img/; multipleInputs:true; align: center; maxLength: 4; label: Enter Password; labelColor: black"
+      "imagePath:/static/img/; multipleInputs:true; align: center; maxLength: 4; label: Enter Password; labelColor: black; model:" +
+        data.model
     );
-    el.getAttribute("super-keyboard").model = data.model;
     el.setAttribute("scale", { x: 20, y: 20, z: 20 });
     el.setAttribute("position", { x: 0, y: 0, z: 0.5 });
 

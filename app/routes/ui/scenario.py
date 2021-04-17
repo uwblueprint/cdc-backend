288d03167c8288c75ef65dbf4c3c9a7090f68baa
Dhruvin Balar <dhruvinb123@gmail.com>
2021-04-17 18:40:49 -0400

    Added admin/scene/{scene_id} path.

--- app/routes/ui/scenario.py
+++ app/routes/ui/scenario.py
@@ -62,7 +62,7 @@ class UIScenarioHandler(BaseUIHandler):
             await self.render(
                 "scene.html",
                 is_last_scene=is_last_scene,
-                scenario_dict=scenario_obj.as_dict(),
+                scenario_name=scenario_obj.name,
                 scene_dict=scene_dict,
                 asset_prefix_url=config.get("asset.prefix_url"),
                 navmesh_src=navmesh_src,
